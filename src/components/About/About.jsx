bea4a299f21fdad65b38709d5ff727ccc508dc17
Ayush <ayushguptass14@gmail.com>
2023-10-09 21:38:58 +0530

    kk

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,14 +3,22 @@ import React, { useEffect, useState } from 'react'
 export default function About() {
 
   const [name, setname]=useState('ss');
- useEffect(()=>{
-    fetch( 'https://asd-er7g.onrender.com/about')
-    .then((response) => {
-        console.log(response);
-        setname(response.status);
-        }).catch((error) => console.error("Error fetching data:", error));
- })
-
+  useEffect(() => {
+    fetch('https://asd-er7g.onrender.com/about')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json(); // Assuming the response is JSON
+      })
+      .then((data) => {
+        // Now 'data' contains the JSON response from your backend
+        console.log(data);
+        // Do something with 'data', such as updating state
+        setname(data.name);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
 
   return (
       <div className="py-16 bg-white">
